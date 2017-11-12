838b0c3b9baf249b63a23fe944b6a6306ba6cbab
Herb Bowie <hbowie@mac.com>
2017-11-12 12:08:40 -0800

    Java Packager Update for 4.1.2

--- javapackager.sh
+++ javapackager.sh
@@ -28,7 +28,7 @@ $jdk/bin/javapackager -deploy \
     -appclass com.powersurgepub.notenik2.Notenik \
     -name Notenik \
     -title "Note taking app" \
-    -BappVersion=4.11 \
+    -BappVersion=4.12 \
     -outdir deploy \
     -outfile notenik \
     -v
